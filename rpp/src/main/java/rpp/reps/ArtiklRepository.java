36c20cdf3ad84ee8a8c5ec736a47da1c8ebf68e8
MiroslavStefanovic <mstef@uns.ac.rs>
2020-03-18 19:02:01 +0100

    Vežbe 03

---
+++ rpp/src/main/java/rpp/reps/ArtiklRepository.java
@@ -0,0 +1,17 @@
+package rpp.reps;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import rpp.jpa.Artikl;
+
+/*
+ * Kao i kada želimo da neku postojeću klasu proširimo nekim novim metodama
+ * i u ovom slučaju želimo da metode koje su deklarisane unutar interfejsa
+ * JpaRepository proširimo nekim novim. Neke od metoda koje su deklarisane
+ * u JpaRepository su:
+ * List<T> findAll()
+ * getOne(ID id);
+ */
+public interface ArtiklRepository extends JpaRepository<Artikl, Integer>{
+
+}
