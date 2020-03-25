e95ec40b24af0ce478bc040beb28290c4aaa2afd
MiroslavStefanovic <mstef@uns.ac.rs>
2020-03-25 12:53:00 +0100

    Vežbe 04

--- rpp/src/main/java/rpp/reps/ArtiklRepository.java
+++ rpp/src/main/java/rpp/reps/ArtiklRepository.java
@@ -1,5 +1,9 @@
 package rpp.reps;
 
+import java.util.Collection;
+
+import javax.persistence.NamedQuery;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import rpp.jpa.Artikl;
@@ -13,5 +17,17 @@ import rpp.jpa.Artikl;
  * getOne(ID id);
  */
 public interface ArtiklRepository extends JpaRepository<Artikl, Integer>{
+	
+	/* Vežbe 04 - JPA omogućava manuelno kreiranje upita, kao što je npr. kreiran upit
+	 * sa anotacijom @NamedQuery u klasi Artikal, ali i kreiranje upita na osnovu naziva
+	 * metoda. Postoji određen broj rezervisanih reči koje možete koristiti u nazivima 
+	 * metode, a koje če omogućiti da se iz samog naziva automatski generiše upit. 
+	 * Neke od rezervisanih reči su npr. StartsWith, EndsWith, NotContaining, Containing, 
+	 * Contains. And, After, Like, OrderBy itd. U konkretnom slučaju upotrebom rezervisanih 
+	 * reči definisana je metoda koja će pronalaziti sve artikle koja u nezivu sadrži prosleđeni 
+	 * String ingorišući da li ja napisan malim ili valikim slovima.
+	 */
+	
+	Collection<Artikl> findByNazivContainingIgnoreCase(String naziv);
 
 }
