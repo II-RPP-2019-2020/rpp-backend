b72e98320f182cba1f5f074599a66283f5a9611d
Miroslav Stefanović <mstef@uns.ac.rs>
2021-04-23 09:18:34 +0200

    Small changes

--- rpp/src/main/java/rpp/ctrl/ArtiklRestController.java
+++ rpp/src/main/java/rpp/ctrl/ArtiklRestController.java
@@ -1,12 +1,12 @@
 package rpp.ctrl;
 
+import java.net.URI;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +21,7 @@ import rpp.reps.ArtiklRepository;
 
 @RestController
 public class ArtiklRestController {
-	
+
 	/*
 	 * Anotacija @Autowired se može primeniti nad varijablama instace, setter metodama i
 	 * konstruktorima. Označava da je neophodno injektovati zavisni objekat. Prilikom
@@ -29,148 +29,147 @@ public class ArtiklRestController {
 	 * koje označavaju da je potrebno kreirati objekte. Upotrebom @Autowired anotacije
 	 * stavljeno je do znanja da je potrebno kreirati objekta klase koja će implementirati
 	 * repozitorijum AriklRepository i proslediti klasi ArtiklRestController referencu
-	 * na taj objekat. 
+	 * na taj objekat.
 	 */
 	@Autowired
 	private ArtiklRepository artiklRepository;
-	
-	/* Vežbe 04 - JdbcTemplate klasa pojednostavljuje korišćenje JDBC (Java Database 
+
+	/* Vežbe 04 - JdbcTemplate klasa pojednostavljuje korišćenje JDBC (Java Database
 	 * Connectivity), objekat ove klase će se koristiti u metodi delete() i zato je
 	 * deklarisan i anotiran sa @Autowired
 	 */
-	
+
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
 	/*
 	 * HTTP GET je jedna od HTTP metoda koja je analogna opciji READ iz CRUD operacija.
 	 * Anotacija @GetMapping se koristi kako bi se mapirao HTTP GET zahtev.
 	 * Predstavlja skraćenu verziju metode @RequestMapping(method = RequestMethod.GET)
 	 * U konkretnom slučaju HTTP GET zahtevi (a to je npr. svako učitavanje stranice u
 	 * browser-u) upućeni na adresu localhost:8083/artikl biće prosleđeni ovoj metodi.
-	 * 
+	 *
 	 * Poziv metode artiklRepository.findAll() će vratiti kolekciju koja sadrži sve
 	 * artikala koji će potom u browseru biti prikazani u JSON formatu
 	 */
-	
-	@CrossOrigin
+
+	//@CrossOrigin
 	@ApiOperation(value = "Returns collection of all Artikl from database.")
 	@GetMapping("artikl")
 	public Collection<Artikl> getAll(){
 		return artiklRepository.findAll();
 	}
-	
+
 	/*
 	 * U slučaju metode getOne(), novina je uvođenje promenljive koja je predstavljena kao
 	 * {id} u @GetMapping("artikl/{id}"). Mapiranje promenljive u vrednost koja se prosleđuje
 	 * konkretnoj metodi getOne() vrši se upotrebom anotacije @PathVariable.
 	 * U konkretnom slučaju HTTP GET zahtev upućen na adresu localhost:8083/artikl/1 biće
 	 * prosleđen ovoj metodi, a vrednost 1 kao ID.
-	 * 
+	 *
 	 *  Poziv metode artiklRepositorz.getOne(id) će vratiti konkretan artikal sa datim ID-je
-	 *  i taj artikal će potom biti prikazan u browseru u JSON formatu. 
+	 *  i taj artikal će potom biti prikazan u browseru u JSON formatu.
 	 */
-	
+
 	/*
 	 * Vežbe 04
 	 */
-	
-	@CrossOrigin
+
+	//@CrossOrigin
 	@ApiOperation(value = "Returns Artikl with id that was forwarded as path variable.")
 	@GetMapping("artikl/{id}")
 	//public Artikl getOne(@PathVariable("id") Integer id) {
 	public ResponseEntity<Artikl> getOne(@PathVariable("id") Integer id) {
-		
+
 		if (artiklRepository.findById(id).isPresent()) {
 			Artikl artikl = artiklRepository.getOne(id);
 			return new ResponseEntity<>(artikl, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 		}
-		
+
 	}
-	
+
 	/* Vežbe 04 - Pored dve postojeće GET metode sa prethodnih vežbi, dodata je još jedna
 	 * metoda koja će koristiti ranije definisanu metodu u ArtiklRepository
-	 * 
+	 *
 	 */
-	
-	@CrossOrigin
+
+	//@CrossOrigin
 	@ApiOperation(value = "Returns Artikl with name that was forwarded as path variable.")
 	@GetMapping("artikl/naziv/{naziv}")
 	public Collection<Artikl> getByNaziv(@PathVariable("naziv") String naziv){
 		return artiklRepository.findByNazivContainingIgnoreCase(naziv);
 	}
-	
+
 	/*
 	 * HTTP POST je jedna od HTTP metoda koja je analogna opciji CREATE iz CRUD operacija.
 	 * Anotacija @PostMapping se koristi kako bi se mapirao HTTP POST zahtev.
 	 * Predstavlja skraćenu verziju metode @RequestMapping(method = RequestMethod.POST)
-	 * U konkretnom slučaju HTTP POST zahtevi upućeni na adresu localhost:8083/artikl 
+	 * U konkretnom slučaju HTTP POST zahtevi upućeni na adresu localhost:8083/artikl
 	 * biće prosleđeni ovoj metodi.
 	 * Poziv metode artiklRepository.save(artikl) će sačuvati prosleđeni artikl u bazi
 	 * podataka
 	 */
-	
-	@CrossOrigin
+
+	//@CrossOrigin
 	@ApiOperation(value = "Adds instance of Artikl to database.")
 	@PostMapping("artikl")
-	public ResponseEntity<HttpStatus> addArtikl(@RequestBody Artikl artikl) {
-		artiklRepository.save(artikl);
-		
-		return new ResponseEntity<HttpStatus>(HttpStatus.CREATED);
+	public ResponseEntity<Artikl> addArtikl(@RequestBody Artikl artikl) {
+		Artikl savedArtikl = artiklRepository.save(artikl);
+		URI location = URI.create("/artikl/" + savedArtikl.getId());
+		return ResponseEntity.created(location).body(savedArtikl);
 	}
-	
+
 	/*
 	 * HTTP PUT je jedna od HTTP metoda koja je analogna opciji UPDATE iz CRUD operacija.
 	 * Anotacija @PutMapping se koristi kako bi se mapirao HTTP PUT zahtev.
 	 * Predstavlja skraćenu verziju metode @RequestMapping(method = RequestMethod.PUT)
-	 * U konkretnom slučaju HTTP PUT zahtevi upućeni na adresu localhost:8083/artikl/{id} 
+	 * U konkretnom slučaju HTTP PUT zahtevi upućeni na adresu localhost:8083/artikl/{id}
 	 * biće prosleđeni ovoj metodi.
 	 * Poziv metode artiklRepository.save(artikl) će izmeniti artikl sa prosleđenim ID-ijem
 	 * i prosleđenim sadržajem u bazi podataka.
 	 */
-	
-	@CrossOrigin
+
+	//@CrossOrigin
 	@ApiOperation(value = "Updates Artikl that has id that was forwarded as path variable with values forwarded in Request Body.")
-	@PutMapping(value = "artikl/{id}", produces = "application/json; charset=utf-8")
-	public ResponseEntity<HttpStatus> updateArtikl(@RequestBody Artikl artikl, 
-			@PathVariable("id")Integer id){
+	//@PutMapping(value = "artikl/{id}", produces = "application/json; charset=utf-8")
+	@PutMapping(value = "artikl/{id}")
+	public ResponseEntity<Artikl> updateArtikl(@RequestBody Artikl artikl, @PathVariable("id")Integer id){
 		if (artiklRepository.existsById(id)) {
 			artikl.setId(id);
-			artiklRepository.save(artikl);
-			return new ResponseEntity<HttpStatus>(HttpStatus.OK);			
+			Artikl savedArtikl = artiklRepository.save(artikl);
+			return ResponseEntity.ok().body(savedArtikl);
 		}
-		
-		return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);
-		
+
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-	
+
 	/*
 	 * HTTP DELETE je jedna od HTTP metoda koja je analogna opciji DELETE iz CRUD operacija.
 	 * Anotacija @DELETEMapping se koristi kako bi se mapirao HTTP DELETE zahtev.
 	 * Predstavlja skraćenu verziju metode @RequestMapping(method = RequestMethod.DELETE)
-	 * U konkretnom slučaju HTTP PUT zahtevi upućeni na adresu localhost:8083/artikl/{id} 
+	 * U konkretnom slučaju HTTP PUT zahtevi upućeni na adresu localhost:8083/artikl/{id}
 	 * biće prosleđeni ovoj metodi.
 	 * Poziv metode artiklRepository.deleteByID(id) će obrisati artikl sa prosleđenim ID-ije
 	 * iz baze podataka.
 	 */
-	
-	@CrossOrigin
+
+	//@CrossOrigin
 	@ApiOperation(value = "Deletes Artikl with id that was forwarded as path variable.")
 	@DeleteMapping("artikl/{id}")
 	public ResponseEntity<HttpStatus> delete(@PathVariable Integer id){
 		if(id==-100 && !artiklRepository.existsById(id)) {
 			jdbcTemplate.execute("INSERT INTO artikl (\"id\", \"proizvodjac\", \"naziv\") VALUES (-100, 'Test Proizvodjac', 'Test Naziv')");
 		}
-		
+
 		if (artiklRepository.existsById(id)) {
 			artiklRepository.deleteById(id);
 			return new ResponseEntity<HttpStatus>(HttpStatus.OK);
 		}
-		
+
 		return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);
-		
+
 	}
-	
+
 }
--- rpp/src/main/java/rpp/ctrl/StavkaPorudzbineRestController.java
+++ rpp/src/main/java/rpp/ctrl/StavkaPorudzbineRestController.java
@@ -24,46 +24,46 @@ import rpp.reps.StavkaPorudzbineRepository;
 
 @RestController
 public class StavkaPorudzbineRestController {
-	
+
 	@Autowired
 	private StavkaPorudzbineRepository stavkaPorudzbineRepository;
-	
+
 	@Autowired
 	private PorudzbinaRepository porudzbinaRepository;
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	@CrossOrigin
 	@ApiOperation(value = "Returns collection of all StavkaPorudzbine from database.")
 	@GetMapping("stavkaPorudzbine")
 	public Collection<StavkaPorudzbine> getAll() {
 		return stavkaPorudzbineRepository.findAll();
 	}
-	
+
 	@CrossOrigin
 	@ApiOperation(value = "Returns StavkaPorudzbine with id that was forwarded as path variable.")
 	@GetMapping("stavkaPorudzbine/{id}")
 	public StavkaPorudzbine getOne(@PathVariable("id") Integer id) {
 		return stavkaPorudzbineRepository.getOne(id);
 	}
-	
+
 	@CrossOrigin
 	@ApiOperation(value = "Returns collection of StavkePorudzbine for Porudzbina with id that was forwarded as path variable.")
 	@GetMapping("stavkaZaPorudzbinu/{id}")
 	public Collection<StavkaPorudzbine> getAllForPorudzbina(@PathVariable("id") Integer id){
 		Porudzbina p = porudzbinaRepository.getOne(id);
 		return stavkaPorudzbineRepository.findByPorudzbina(p);
-		
+
 	}
-	
+
 	@CrossOrigin
 	@ApiOperation(value = "Returns collection of StavkePorudzbine with price that is lower then price that was forwarded as path variable.")
 	@GetMapping(value = "stavkaPorudzbineCena/{cena}")
 	public Collection<StavkaPorudzbine> getStavkaPorudzbineCena(@PathVariable("cena") BigDecimal cena){
 		return stavkaPorudzbineRepository.findByCenaLessThanOrderById(cena);
 	}
-	
+
 	@CrossOrigin
 	@ApiOperation(value = "Adds instance of StavkaPorudzbine to database.")
 	@PostMapping("stavkaPorudzbine")
@@ -72,7 +72,7 @@ public class StavkaPorudzbineRestController {
 		stavkaPorudzbineRepository.save(stavkaPorudzbine);
 		return new ResponseEntity<HttpStatus>(HttpStatus.CREATED);
 	}
-	
+
 	@CrossOrigin
 	@ApiOperation(value = "Updates StavkaPorudzbine that has id that was forwarded as path variable with values forwarded in Request Body.")
 	@PutMapping("stavkaPorudzbine/{id}")
@@ -84,23 +84,23 @@ public class StavkaPorudzbineRestController {
 		stavkaPorudzbineRepository.save(stavkaPorudzbine);
 		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
 	}
-	
+
 	@CrossOrigin
 	@ApiOperation(value = "Deletes StavkaPorudzbine with id that was forwarded as path variable.")
 	@DeleteMapping("stavkaPorudzbine/{id}")
 	public ResponseEntity<HttpStatus> delete(@PathVariable Integer id){
 		if (id == -100 && !stavkaPorudzbineRepository.existsById(-100)) {
-            
-            jdbcTemplate.execute("INSERT INTO stavka_porudzbine (\"id\", \"redni_broj\", \"kolicina\", \"jedinica_mere\", \"cena\", \"porudzbina\", \"artikl\") "
-                    + "VALUES ('-100', '100', '1', 'kom', '1', '1', '1')");
-        }
-		
+
+			jdbcTemplate.execute("INSERT INTO stavka_porudzbine (\"id\", \"redni_broj\", \"kolicina\", \"jedinica_mere\", \"cena\", \"porudzbina\", \"artikl\") "
+					+ "VALUES ('-100', '100', '1', 'kom', '1', '1', '1')");
+		}
+
 		if(stavkaPorudzbineRepository.existsById(id)) {
-            stavkaPorudzbineRepository.deleteById(id); 
-            return new ResponseEntity<HttpStatus>(HttpStatus.OK);
-        }
-         
-        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+			stavkaPorudzbineRepository.deleteById(id);
+			return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+		}
+
+		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
 	}
 
 }
